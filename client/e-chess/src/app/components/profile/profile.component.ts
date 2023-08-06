ee1f8963205e737233c81e5f33e5e8231614ea52
Simo23-cpp <CCCSMN01T19F892H@studium.unict.it>
2023-08-06 17:24:54 +0200

    set time game in homepage

--- client/e-chess/src/app/components/profile/profile.component.ts
+++ client/e-chess/src/app/components/profile/profile.component.ts
@@ -8,16 +8,28 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent {
 
+  timeGame: number = 0;
+  showAlert: boolean = false;
+
   constructor(private router: Router) { }
 
   play() {
-    console.log("play");
-    this.router.navigate(["/game"]);
+    console.log(this.timeGame);
+    if (this.timeGame != 0) {
+      this.router.navigate(["/game"]);
+    }
+    else {
+      this.showAlert = true;
+    }
   }
 
   logout() {
     console.log("logout");
     this.router.navigate(["/login"]);
+  }
+
+  setTime(time: number) {
+    this.timeGame = time;
   }
 
 }
